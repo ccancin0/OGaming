29ad8c823a55db8afaa76ff9925d566393c5e9d0
ccancin0 <ccancino@memphis.edu>
2017-07-18 14:44:21 -0500

    login and sign up forms working with validation

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
   define('ROOT_URL', 'https://localhost/OG/');
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
-  define('DB_PASS', 'turtle12');
-  define('DB_NAME', 'ogaming');
+  define('DB_PASS', 'pass');
+  define('DB_NAME', 'db');
